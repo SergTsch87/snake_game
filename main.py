76e9d68cf14dbc04a9dabf144fc2d80b6d59da3b
SergTsch87 <sergtsch87@gmail.com>
2024-09-26 17:09:33 +0300

    Створив клас Player та його метод init у main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 #!usr/bin/env
+import pygame
+from pygame.sprite import _Group
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        # Замість зображення використовуємо кольоровий прямокутник
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((0, 128, 255))   # Задаємо колір
+        self.rect = self.image.get_rect()
+        self.rect.center = (400, 300)
 
 
 def main():
